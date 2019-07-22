302a27efdab36addb4961109dff3675790206c46
abullahi <49534483+huah1600@users.noreply.github.com>
2019-07-22 13:44:05 +0300

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,4 @@
+
+vector<person> read_file(string filename);
+size_t find_in_names(vector<person> persons, string name_part);
+vector<person> find_person_from_city(vector<person> find_citylock, string city);
